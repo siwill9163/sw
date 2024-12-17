f3d877b975149d7647b83395657bc16510c42892
siwill9163 <simon.williams1985@outlook.com>
2024-12-17 13:23:52 +0000

    4th

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base : "/sw/",
+  
   plugins: [react()],
-  optimizeDeps: {
-    include: ['react-simple-typewriter'],
-  },
+  base : "/sw/",
+  
 })
